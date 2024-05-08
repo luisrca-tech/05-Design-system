7bc3fa0277725d08bdd79722ec52f1ed312ef795
Luis Felipe <luisrochacruzalves@gmail.com>
2024-05-08 00:06:17 -0300

    feat: Add core components

---
+++ packages/docs/.storybook/preview.ts
@@ -0,0 +1,21 @@
+import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      theme: themes.dark
+    },
+    backgrounds: {
+      default: 'dark'
+    }
+  },
+};
+
+export default preview;
